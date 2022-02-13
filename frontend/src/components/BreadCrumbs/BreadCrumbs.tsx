8acf7def2c0649e336664de7b900465961fa0dd9
Mauricio Viera <mauricio.viera6@gmail.com>
2022-02-13 22:27:44 +0100

    fix BreadCumbs component

--- frontend/src/components/BreadCrumbs/BreadCrumbs.tsx
+++ frontend/src/components/BreadCrumbs/BreadCrumbs.tsx
@@ -1,16 +1,14 @@
 import { FC } from 'react'
 import { Link } from 'react-router-dom'
-import { useUiDispatch } from 'finpok/store/ui/UiProvider'
 
 const BreadCrumbs: FC = () => {
-  const { closeOwnedCryptoDetail } = useUiDispatch()
   return (
     <section className="text-sm breadcrumbs">
       <ul>
         <li>
           <Link to="/">Home</Link>
         </li>
-        <li onClick={() => closeOwnedCryptoDetail()}>
+        <li>
           <Link to="/portfolio">Portfolio</Link>
         </li>
       </ul>
