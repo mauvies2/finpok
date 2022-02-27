fe182a44158deb6cd23a12df6b95f7dc2b70bf27
Mauricio Viera <mauricio.viera6@gmail.com>
2022-02-27 18:06:56 +0100

    fix config and env variables

--- api/src/config/default.ts
+++ api/src/config/default.ts
@@ -7,20 +7,20 @@ const config = {
     cryptocurrencies: process.env.COINMARKETCAP_URL || 'https://pro-api.coinmarketcap.com/v1',
   },
 
-  mongoDatabase: process.env.MONGO_DATABASE || 'finpokdb',
+  mongoDatabase: process.env.MONGO_DATABASE,
 
-  mongoUser: process.env.MONGO_USER || 'finpok',
+  mongoUser: process.env.MONGO_USER,
 
-  mongoPassword: process.env.MONGO_PASSWORD || 'finpok123',
+  mongoPassword: process.env.MONGO_PASSWORD,
 
   mongoHost:
     process.env.MONGO_HOST || process.env.NODE_ENV === 'dev' ? 'finpok.tvwve.mongodb.net' : 'finpok.tvwve.mongodb.net',
 
-  mongoPort: process.env.MONGO_HOST || process.env.NODE_ENV === 'dev' ? 27017 : 27015,
+  mongoPort: process.env.NODE_ENV === 'dev' ? 27017 : 27015,
 
   port: process.env.PORT || 5000,
 
-  jwt: 'secret',
+  jwt: process.env.JWT_SECRET,
 }
 
 export default config
