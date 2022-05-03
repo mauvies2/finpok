3fd996c4f2bd05918c7c129156932e772dac9aa4
Mauricio Viera <mauricio.viera6@gmail.com>
2022-05-03 19:32:13 +0200

    only home and portfolio working

--- frontend/src/components/BreadCrumbs/BreadCrumbs.tsx
+++ frontend/src/components/BreadCrumbs/BreadCrumbs.tsx
@@ -1,16 +1,26 @@
 import { FC } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 const BreadCrumbs: FC = () => {
+  const { pathname } = useLocation()
+
+  const locations = pathname.split('/').slice(1)
+
   return (
     <section className="breadcrumbs text-sm">
       <ul>
         <li>
           <Link to="/">Home</Link>
         </li>
-        <li>
-          <Link to="/portfolio">Portfolio</Link>
-        </li>
+        {locations.map((loc) => (
+          <li key={loc}>
+            {loc === 'portfolio' ? (
+              <Link to="/portfolio">{loc.replace(loc[0], loc[0].toUpperCase())}</Link>
+            ) : (
+              <div>{loc.replace(loc[0], loc[0].toUpperCase())}</div>
+            )}
+          </li>
+        ))}
       </ul>
     </section>
   )
