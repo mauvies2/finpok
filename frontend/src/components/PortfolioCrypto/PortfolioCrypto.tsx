50f4d6cce65957052076f005ede8eaf995210325
Mauricio Viera <mauricio.viera6@gmail.com>
2022-05-15 14:37:56 +0200

    fix PortfolioCrypto row clicking

--- frontend/src/components/PortfolioCrypto/PortfolioCrypto.tsx
+++ frontend/src/components/PortfolioCrypto/PortfolioCrypto.tsx
@@ -13,7 +13,7 @@ interface PortfolioCryptoProps {
 }
 
 const PortfolioCrypto: FC<PortfolioCryptoProps> = ({ ownedCrypto, crypto }) => {
-  const { selectOwnedCryptoDetail } = useUiDispatch()
+  const { selectOwnedCryptoDetail, openModal } = useUiDispatch()
 
   if (!ownedCrypto || !crypto) return null
 
@@ -27,53 +27,58 @@ const PortfolioCrypto: FC<PortfolioCryptoProps> = ({ ownedCrypto, crypto }) => {
   })
 
   return (
-    <Link
-      className="flex border-b py-5"
-      to={`/portfolio/${ownedCrypto.symbol}`}
-      onClick={() => selectOwnedCryptoDetail(ownedCrypto.symbol)}
-    >
-      <div className="flex flex-1  items-center">
-        <img src={crypto.logoUrl} className="mr-3" width="20" alt="logo" />
-        <div>
-          <p className="font-bold">{ownedCrypto.name}</p>
-          <p>{ownedCrypto.symbol}</p>
+    <div className="flex border-b">
+      <Link
+        className="flex flex-1 py-5"
+        to={`/portfolio/${ownedCrypto.symbol}`}
+        onClick={() => selectOwnedCryptoDetail(ownedCrypto.symbol)}
+      >
+        <div className="flex flex-1 items-center">
+          <img src={crypto.logoUrl} className="mr-3" width="20" alt="logo" />
+          <div>
+            <p className="font-bold">{ownedCrypto.name}</p>
+            <p>{ownedCrypto.symbol}</p>
+          </div>
         </div>
-      </div>
-      <div className="flex-1 justify-end text-right font-semibold">
-        <p className="mb-1">{formatNumber(cryptoPrices.price, { symbol: '$', fractionDigits: 2 })}</p>
-        <p className={`${change24hStyle} lg:hidden`}>{change24h}</p>
-      </div>
-      <div className="hidden flex-1 justify-end text-right font-semibold md:block">
-        <p className={change24hStyle}>{change24h}</p>
-      </div>
+        <div className="flex-1 items-center justify-end text-right font-semibold">
+          <p className="mb-1">{formatNumber(cryptoPrices.price, { symbol: '$', fractionDigits: 2 })}</p>
+          <p className={`${change24hStyle} lg:hidden`}>{change24h}</p>
+        </div>
+        <div className="hidden flex-1  text-right font-semibold md:block">
+          <p className={change24hStyle}>{change24h}</p>
+        </div>
 
-      <div className="flex-1 cursor-pointer justify-end text-right">
-        <p className="mb-1 font-semibold">
-          {cryptoPrices.price
-            ? formatNumber(ownedCrypto.amount * cryptoPrices.price, {
-                symbol: '$',
-                fractionDigits: 2,
-                sign: ownedCrypto.amount > 0 ? undefined : false,
-                abs: true,
-              })
-            : 0}
-        </p>
-        <p className="text-xs">
-          {formatNumber(ownedCrypto.amount, {
-            maximumSignificantDigits: 4,
-            unit: ownedCrypto.symbol,
-            sign: ownedCrypto.amount > 0 ? undefined : false,
-          })}
-        </p>
-      </div>
-      <div className="hidden flex-1 justify-end text-right font-semibold md:block">
-        <p className="mb-1">{formatNumber(ownedCrypto.buyAvgPrice, { symbol: '$', fractionDigits: 2 })}</p>
-      </div>
+        <div className="flex-1 cursor-pointer text-right">
+          <p className="mb-1 font-semibold">
+            {cryptoPrices.price
+              ? formatNumber(ownedCrypto.amount * cryptoPrices.price, {
+                  symbol: '$',
+                  fractionDigits: 2,
+                  sign: ownedCrypto.amount > 0 ? undefined : false,
+                  abs: true,
+                })
+              : 0}
+          </p>
+          <p className="text-xs">
+            {formatNumber(ownedCrypto.amount, {
+              maximumSignificantDigits: 4,
+              unit: ownedCrypto.symbol,
+              sign: ownedCrypto.amount > 0 ? undefined : false,
+            })}
+          </p>
+        </div>
+        <div className="hidden flex-1 text-right font-semibold md:block">
+          <p className="mb-1">{formatNumber(ownedCrypto.buyAvgPrice, { symbol: '$', fractionDigits: 2 })}</p>
+        </div>
+      </Link>
       <div className="hidden items-center justify-end text-right font-semibold md:flex md:w-24">
-        <PlusLg className="h-[15px] w-[15px] text-gray-400" />
-        <MoreVertical className="ml-3 h-[21px] w-[21px] text-gray-400" />
+        <PlusLg
+          className="h-[15px] w-[15px] cursor-pointer text-gray-400"
+          onClick={() => openModal(`/portfolio/transaction-operation/${ownedCrypto.symbol}`)}
+        />
+        <MoreVertical className="ml-3 h-[21px] w-[21px] cursor-pointer text-gray-400" />
       </div>
-    </Link>
+    </div>
   )
 }
 
