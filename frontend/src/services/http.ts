c060f83b9fb10c9ae349e7a935a45abca122db99
Mauricio Viera <mauricio.viera6@gmail.com>
2022-08-03 15:50:49 +0200

    prueba

--- frontend/src/services/http.ts
+++ frontend/src/services/http.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? 'http://finpoq.com' : 'http://localhost:5000',
+  // baseURL: process.env.NODE_ENV === 'production' ? 'http://finpoq.com' : 'http://localhost:5000',
+  baseURL: 'http://localhost:5000',
   timeout: 10000,
   headers: {
     Accept: 'application/json',
