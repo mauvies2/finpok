d349d4be7a0d6998c7b1ffe1eddef08a59026822
Mauricio Viera <mauricio.viera6@gmail.com>
2022-08-11 17:26:49 +0200

    make app pwa

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,38 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Finpoq',
+        short_name: 'Finpoq',
+        description: 'Financial pocket',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable any',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable any',
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     host: true,
     port: 3000,
